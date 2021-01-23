81db5370e13b626cadd32408d29d51b952327f38
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-01-23 18:11:44 +0900

    feat: first commit

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import BookingCalendar from 'react-native-booking-calendar';
 
-
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
